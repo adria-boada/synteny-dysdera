0360ebb1fe003881d3e56534a48d62d945b046aa
adria-boada <94541540+adria-boada@users.noreply.github.com>
2022-06-12 13:13:04 +0200

    shell-scripts which automatically analyze files.

--- dcatchr_piechart.sh
+++ dcatchr_piechart.sh
@@ -1,19 +1,19 @@
 
 #! bin/bash
 
-repeat_file="~/"
-idx_file=""
+repeat_file="/home/adria/Escritorio/Data/RepeatMasker/Dcat35_V0.92.fasta_renamed2_RMclasses.out"
+idx_file="/home/adria/Escritorio/Data/RepeatMasker/Dcat09_final_multiline_0.91.fasta.idx"
 
 # per a tots els cromosomes principals:
-for crm in dcatchr1 dcatchr2 dcatchr3 dcatchr4 dcatchr5 dcatchrx; do
+for crm in DcatChr1 DcatChr2 DcatChr3 DcatChr4 DcatChr5 DcatChrx; do
 	# create a table file
-	python3 ~/Dipogits/synteny-dysdera/repeatmasker_parser3.py "$repeat_file" dsil "$crm" > tbl
+	python3 ~/Dipogits/synteny-dysdera/repeatmasker_parser3.py "$repeat_file" dcat35 "$crm" > tbl
 	# show the table to the terminal (least its head:)
 	head tbl
 	# read the table file with piechart.py and create a piechart.
 	python3 ~/Dipogits/synteny-dysdera/repeatmasker_piechart.py tbl -i "$idx_file" -c "$crm"
 	# move the output
-	mv piechart.png "${crm}_piechart.png"
+	mv pchart.png "${crm}_piechart.png"
 done
 
 # remove tmp tble files...
--- dsilchr_piechart.sh
+++ dsilchr_piechart.sh
@@ -1,11 +1,11 @@
 
 #! bin/bash
 
-repeat_file="~/"
-idx_file=""
+repeat_file="/home/adria/Escritorio/Data/RepeatMasker/Dsil_V2.3.fasta.renamed2_RMclasses.out"
+idx_file="/home/adria/Escritorio/Data/RepeatMasker/Dsil_V2.3.fasta.idx"
 
 # per a tots els cromosomes principals:
-for crm in dsilchr1 dsilchr2 dsilchr3 dsilchr4 dsilchr5 dsilchr6 dsilchru1 dsilchru2 dsilchrx; do
+for crm in DsilChr1 DsilChr2 DsilChr3 DsilChr4 DsilChr5 DsilChr6 DsilChru1 DsilChru2 DsilChrx; do
 	# create a table file
 	python3 ~/Dipogits/synteny-dysdera/repeatmasker_parser3.py "$repeat_file" dsil "$crm" > tbl
 	# show the table to the terminal (least its head:)
@@ -13,7 +13,7 @@ for crm in dsilchr1 dsilchr2 dsilchr3 dsilchr4 dsilchr5 dsilchr6 dsilchru1 dsilc
 	# read the table file with piechart.py and create a piechart.
 	python3 ~/Dipogits/synteny-dysdera/repeatmasker_piechart.py tbl -i "$idx_file" -c "$crm"
 	# move the output
-	mv piechart.png "${crm}_piechart.png"
+	mv pchart.png "${crm}_piechart.png"
 done
 
 # remove tmp tble files...
