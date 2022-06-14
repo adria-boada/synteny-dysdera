010166c4a8ec54c9254404b98d435e7f18f89ce8
adria-boada <94541540+adria-boada@users.noreply.github.com>
2022-06-14 21:19:14 +0200

    Changed input from shell-scripts
    Now the vars have to be inputted in the terminal.

--- dcatchr_piechart.sh
+++ dcatchr_piechart.sh
@@ -1,8 +1,8 @@
 
 #! bin/bash
 
-repeat_file="/home/adria/Escritorio/Data/RepeatMasker/Dcat35_V0.92.fasta_renamed2_RMclasses.out"
-idx_file="/home/adria/Escritorio/Data/RepeatMasker/Dcat09_final_multiline_0.91.fasta.idx"
+repeat_file="$1"
+idx_file="$2"
 
 # per a tots els cromosomes principals:
 for crm in DcatChr1 DcatChr2 DcatChr3 DcatChr4 DcatChr5 DcatChrx Scaffold; do
--- dsilchr_piechart.sh
+++ dsilchr_piechart.sh
@@ -1,8 +1,8 @@
 
 #! bin/bash
 
-repeat_file="/home/adria/Escritorio/Data/RepeatMasker/Dsil_V2.3.fasta.renamed2_RMclasses.out"
-idx_file="/home/adria/Escritorio/Data/RepeatMasker/Dsil_V2.3.fasta.idx"
+repeat_file="$1"
+idx_file="$2"
 
 # per a tots els cromosomes principals:
 for crm in DsilChr1 DsilChr2 DsilChr3 DsilChr4 DsilChr5 DsilChr6 DsilChru1 DsilChru2 DsilChrx Scaffold; do
