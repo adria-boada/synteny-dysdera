8c848a8fdcb51a1a5823f810054406585568e511
adria-boada <94541540+adria-boada@users.noreply.github.com>
2022-11-30 18:36:15 +0100

    HR-bam-indexing
    Creates an index and randomly samples 100 mappings (lines)

---
+++ Submissio/bam_indexing.sh
@@ -0,0 +1,15 @@
+
+#! /bin/bash
+
+source /users-d3/adria.boada/.bashrc
+echo "bamtools path: $(which bamtools)"
+
+echo "arg-one: bam mapping/alignment that should be indexed"
+echo "  --->  $1"
+
+# index bamfile.
+bamtools index $1
+
+# randomly sample fragments from it.
+bamtools random -in $1 -out "sampled100_$1" -n 100
+
