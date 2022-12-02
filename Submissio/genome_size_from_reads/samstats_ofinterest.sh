226e6c02a14da9300254d5c99cf47b3ec3e0d0a2
adria-boada <94541540+adria-boada@users.noreply.github.com>
2022-12-02 18:50:25 +0100

    Bugfix#1
    Samstats_ofinterest should be inputted the fullname. It previously added
    two extensions and couldn't proceed normally.

--- Submissio/genome_size_from_reads/samstats_ofinterest.sh
+++ Submissio/genome_size_from_reads/samstats_ofinterest.sh
@@ -8,23 +8,23 @@ fn=$1
 
 # Filtra els stats generals dins el fitxer .stats.tmp
 # i extreu-ne les dades d'interès.
-qr=$(grep "^SN" ${fn}.stats.tmp|cut -f2-|grep "raw total sequences")
+qr=$(grep "^SN" $fn|cut -f2-|grep "raw total sequences")
 echo "+ Number of raw reads queried: $(echo $qr|cut -f4 -d' ')"
-prim=$(grep "primary mapped" ${fn}.stats.tmp|cut -f1 -d' '|tr -d '(')
+prim=$(grep "primary mapped" $fn|cut -f1 -d' '|tr -d '(')
 echo "+ Primary alignments: $prim"
-nprim=$(grep "^SN" ${fn}.stats.tmp|cut -f2-|grep "non-primary alignments")
+nprim=$(grep "^SN" $fn|cut -f2-|grep "non-primary alignments")
 echo "+ Non-primary alignments: $(echo $nprim|cut -f3 -d' ')"
-supp=$(grep "^SN" ${fn}.stats.tmp|cut -f2-|grep "supplementary alignments")
+supp=$(grep "^SN" $fn|cut -f2-|grep "supplementary alignments")
 echo "+ Supplementary alignments: $(echo $supp|cut -f3 -d' ')"
-nuc=$(grep "^SN" ${fn}.stats.tmp|cut -f2-|grep "total length:")
+nuc=$(grep "^SN" $fn|cut -f2-|grep "total length:")
 echo "+ Raw reads' cumulative length: $(echo $nuc|cut -f3 -d' ')"
-nuc=$(grep "^SN" ${fn}.stats.tmp|cut -f2-|grep "(cigar):")
+nuc=$(grep "^SN" $fn|cut -f2-|grep "(cigar):")
 echo "+ Bases mapped (more accurate): $(echo $nuc|cut -f4 -d' ')"
-err=$(grep "^SN" ${fn}.stats.tmp|cut -f2-|grep "error rate")
+err=$(grep "^SN" $fn|cut -f2-|grep "error rate")
 echo "+ Error rate: $(echo $err|cut -f3 -d' ')"
-insert=$(grep "^SN" ${fn}.stats.tmp|cut -f2-|grep "insert size")
+insert=$(grep "^SN" $fn|cut -f2-|grep "insert size")
 echo "+ Insert size and its standard deviation: $(echo $insert|cut -f4 -d' ') \$\pm\$ $(echo $insert|cut -f9 -d' ')"
-perc=$(grep "primary mapped" ${fn}.stats.tmp|cut -f6 -d' '|tr -d '(')
+perc=$(grep "primary mapped" $fn|cut -f6 -d' '|tr -d '(')
 echo "+ Percentage of primary reads mapped: $perc"
 echo
 
