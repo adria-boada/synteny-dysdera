b905f23f2944082fd56827c687f59e617b9ebc28
adria-boada <94541540+adria-boada@users.noreply.github.com>
2022-12-05 14:24:44 +0100

    Update bwa_index.sh

--- Submissio/bwa_index.sh
+++ Submissio/bwa_index.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
 
-seq_file=$1
+# Indexes the FASTA reference assemblies.
+# It is required in order for bwa mem to work properly.
+# Call the script as follows:
+# bwa_index.sh ref1.fa ref2.fa ... refN.fa
+# It will index each ref.fa passed as argv.
 
-bwa index $seq_file
+# PATH
+echo "Burrows-Wheeler Aligner (bwa)"
+echo "  PATH: $(which bwa)"
+echo
+
+# INDEXING
+while [ "$1" != "" ]; do
+	echo "Indexing $1"
+	bwa index $1
+	shift
+done
+
