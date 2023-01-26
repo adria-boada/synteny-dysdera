508a4ba07d34f4386ecd1993482608a3d71e85b1
adria-boada <94541540+adria-boada@users.noreply.github.com>
2023-01-26 22:10:57 +0100

    Updates and fixes to filter_alignment.sh

--- Submissio/samtools/filter_alignment.sh
+++ Submissio/samtools/filter_alignment.sh
@@ -49,8 +49,8 @@ echo "Threads used: $threads"
 
 for fn in $(echo "$ARGS") ; do
   out="primary_$(basename $fn)"
-  echo -e "Input: $fn\nOutput: primary_${out}"
+  echo -e "Input: $fn\nOutput: ${out}"
   # -F: exclude reads with any flag of 256 or 2048 (their sum)
-  samtools view -@ $threads -bF 2304 $fn > primary_${out}
+  samtools view -@ $threads -bF 2304 $fn > ${out}
 done
 
