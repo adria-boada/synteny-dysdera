cf0f84ede60da566a7b198ddd46579c9bbc34e4d
adria-boada <94541540+adria-boada@users.noreply.github.com>
2023-05-07 12:37:45 +0200

    Update circos_parsegm.R

--- circos_parsegm.R
+++ circos_parsegm.R
@@ -1,48 +1,14 @@
+#! /usr/bin/env Rscript
 
-library(circlize)
-library(reshape2)
-library(colourvalues)
+library(circlize)      # circular plots
+library(colourvalues)  # colouring links depending on variable
 
 ### MANUAL OPTIONS ###
 
-# select a misc. var to represent between...
-# gene number col. -> 7
-# gene length col. -> 8
-# gene distance col. -> 9
-# intra / extra chromosomal -> 10
-misc_var = 10
-
-### READ DATAFRAME ###
-
-df = read.table("segments_linked_pairs.tsv",
-  sep='\t', header=TRUE, row.names=1)
-# create a separate column with selected misc var...
-if (misc_var==7) {
-  misc_var_title=paste('Ratio of gene number',
-  '\n(source vs. destiny segments)')
-  df$var = df[, misc_var]}
-if (misc_var==8) {
-  misc_var_title=paste('Ratio of mean gene lengths',
-  '\n(source vs. destiny segments)')
-  df$var = df[, misc_var]}
-if (misc_var==9) {
-  misc_var_title=paste('Ratio of mean gene distances',
-  '\n(source vs. destiny segments)')
-  df$var = df[, misc_var]}
-if (misc_var==10) {
-  misc_var_title='Whether links are intrachromosomal'
-  # add a bool column which tracks whether links are intra or extra chromosomal
-  df$var = df$sequid_from==df$sequid_to
-}
-
-### CREATE COLOUR COLUMN CORRELATED TO MISC RATIOS ###
-
-# create a column with colours associated to
-# gene number column
-col_ramp = colour_values(c(1, df$var))
-df$col = col_ramp[-1]
-# check the colours with a barplot
-if (FALSE) {barplot(df$var, col=df$col)}
+# Read supplied arguments
+args = commandArgs(trailingOnly=TRUE)
+# Path to segments dataframe file (should be first and only argument)
+segments_file = args[1]
 # manually add chromosome length
 # (would be good to add fasta.idx reading capabilities)
 crm_idx = data.frame(
@@ -53,14 +19,53 @@ crm_idx = data.frame(
         158554970, 152324531, 96325250)
 )
 
+### READ DATAFRAME ###
+
+df = read.table(segments_file,
+  sep='\t', header=TRUE, row.names=1)
+# specify the four legend titles
+leg_titles = c(
+  paste('Ratio of gene number',
+  '\n(source vs. destiny segments)'),
+  paste('Ratio of mean gene lengths',
+  '\n(source vs. destiny segments)'),
+  paste('Ratio of mean gene distances',
+  '\n(source vs. destiny segments)'),
+  'Intrachromosomal'
+)
+
+### COLOUR OF CIRCOS LINKS ###
+
+# specify colour links in data.frame
+df_cols = data.frame(
+  gn=colour_values(c(1, df[, 7])),
+  gl=colour_values(c(1, df[, 8])),
+  gd=colour_values(c(1, df[, 9])),
+  # add a bool column which tracks whether links are intra or extra chromosomal
+  intra=colour_values(c(FALSE, df$sequid_from==df$sequid_to))
+)
+# specify min and max colour values
+col_range = colour_values(c(0,1))
+# specify min and max legend values
+val_range = data.frame(
+      min=c(1, 1, 1, FALSE),
+      max=c(
+        df$gn[df$gn==max(df$gn)],
+        df$gL[df$gL==max(df$gL)],
+        df$gD[df$gD==max(df$gD)],
+        TRUE)
+      )
+
 ### PLOTTING CIRCOS FIGURE ###
 
 # # # # # #
 # export plot to 'Rplot.pdf'
-# increade default width to correctly position legend
+# increase default width to correctly display legend
 pdf(width=9)
 # # # # # #
 
+# Repeat for each column of colours
+for (i in c(1:4)) {
 circos.clear() # good habit to clear previous options
 circos.initialize(sectors=crm_idx$crm,
                   xlim=crm_idx[, c(2,3)])
@@ -77,25 +82,20 @@ circos.track(crm_idx$crm, ylim=c(0,.25),
 )
 # connect 1:3 (origin segment) to 4:6 (recipient segment)
 circos.genomicLink(df[, c(1:3)], df[, c(4:6)],
-  col = df[, length(df)], #h.ratio = .4
+  col = df_cols[-1, i], #h.ratio = .4
 )
 # add a legend with the min and max of misc value,
 # and the colours used to represent these.
-if (misc_var==10) {
-  # TRUE/FALSE intrachr links
-  col_ramp = colour_values(c(0, 1))
-  legend('bottomleft', title=misc_var_title, bty='n',
-    legend = c(FALSE, TRUE),
-    fill = c(col_ramp[1], col_ramp[2]))
-} else {
-  # legend for other misc variables
-  legend('bottomleft', title=misc_var_title, bty='n',
-    legend = c(1, df$var[df$var==max(df$var)]),
-    fill = c(col_ramp[1], df$col[df$var==max(df$var)]))
-}
+  legend('bottomleft', title=leg_titles[i], bty='n',
+    legend = c(val_range$min[i], val_range$max[i]),
+    fill = c(col_range[1], col_range[2]),
+    )
+
+} # The loop for creating multiple plots ends
 
 # # # # # #
 # shut down plotting device
 dev.off()
 # # # # # #
+
 
