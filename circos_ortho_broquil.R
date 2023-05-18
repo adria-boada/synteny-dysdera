db651bad002d034d3bd53e0abfd6b5e29929ae87
adria-boada <94541540+adria-boada@users.noreply.github.com>
2023-05-18 17:18:46 +0200

    Incorporate 2:1 cases in analyses
    Incorporates 2:1 cases as links, instead of binning them like multigenic
    families (non-one-to-one relationship).

--- circos_ortho_broquil.R
+++ circos_ortho_broquil.R
@@ -3,16 +3,17 @@
 
 ### MANUAL OPTIONS and READING DATAFRAMES ###
 
-# Read supplied arguments
+# Read supplied files in argument line
 args = commandArgs(trailingOnly=TRUE)
 if (length(args) < 3) {
+  # help message (if less than the needed args are supplied)
   stop(paste("Supply exactly 3 argument files:",
              "circos_parsegm.R `broquil_orthologs.tsv` `sp1_chr.idx` `sp2_chr.idx`",
              "",
-             "`sp1_chr.idx` must have the following format:",
-             "Species-Legend  sp-brocc.tsv  colours",
-             "Scaffold_1      100           #FFFFFF",
-             "Scaffold_2      50            #EEEEEE",
+             "`sp1_chr.idx` must have the following format:    ",
+             "Species-Legend  sp-brocc.tsv  colours  2ndary-col",
+             "Scaffold_1      100           #FFFFFF  #DDDDDD   ",
+             "Scaffold_2      50            #EEEEEE  #CCCCCC   ",
              "etc.",
              "`sp2_chr.idx` follows the same format but does not require colours",
              sep='\n'),
@@ -23,61 +24,131 @@ orthologs_file = args[1]
 # Path to a tabulated index files
 index_file_color = args[2]
 index_file_cless = args[3]
+# adjust window size for counting multigenic families per window
+window_size = 4e6 # base-pairs
 
 library(circlize)      # circular plots
 library(colourvalues)  # colouring links depending on variable
 
 # pairs and multigenic families of orthologs
-df = read.table(orthologs_file,
+df_input_table = read.table(orthologs_file,
   sep='\t', header=TRUE, row.names=1)
 # index of sequids in the previous file, length-sorted
 idx_col = read.table(index_file_color,
-  sep='\t', header=FALSE)[, c(1:3)]
+  sep='\t', header=FALSE)[, c(1:4)]
 idx_lss = read.table(index_file_cless,
   sep='\t', header=FALSE)[, c(1:2)]
-# rename idx columns
-names(idx_col) = c('crm', 'end', 'col')
+# rename chromosome_index_dataframe's columns
+names(idx_col) = c('crm', 'end', 'col', 'col_2ary')
 names(idx_lss) = c('crm', 'end')
-# read both species names ('first' word in tsv header)
+# read both species complete names ('first' field in tsv header)
+# moreover, read shortened names found in tsv ('second' field in tsv header
 sp_names = c(leg_col=idx_col[1,1], leg_lss=idx_lss[1,1],
              df_col=idx_col[1,2], df_lss=idx_lss[1,2])
-# check that species in idx[1,2] are found in df$Species
-if (!all(sp_names[c(3,4)] %in% unique(df$Species))) {
+# check that species in idx[1,2] for both files are found in df$Species
+if (!all(sp_names[c(3,4)] %in% unique(df_input_table$Species))) {
   stop('ERROR: The species defined in idx[1,2] do not match unique(df$Species)', call.=FALSE)
 }
 # if there are more than two species in dataframe, warn
-if (length(unique(df$Species)) > 2) {
+if (length(unique(df_input_table$Species)) > 2) {
   stop('ERROR: There are more than two species in broccoli df!', call.=FALSE)
 }
 # add a "starting" position for all chr
 idx_col$start = 0
 idx_lss$start = 0
-# and reorder the columns
-highlighting_crm = data.frame(col=idx_col$col[-1])  # store colours in a new var
+# store general colours in a new data.frame
+# pair these colours with scaffolds
+highlighting_crm = data.frame(
+  sequid = 0,  # added later in this paragraph
+  col = idx_col$col[-1],
+  col_2ary = idx_col$col_2ary[-1])
+# and sort the idxs columns
 idx_col = idx_col[, c('crm', 'start', 'end')] # removes colour column
 idx_lss = idx_lss[, c('crm', 'start', 'end')]
 # paste an identifier to all scaffold
-# (to identify dcat scaffold_1 from dtil scaffol_1)
+# (to identify dcat's scaffold_1 from dtil's scaffold_1)
 idx_col$crm = paste0(sp_names[3], '@', idx_col$crm)
 idx_lss$crm = paste0(sp_names[4], '@', idx_lss$crm)
-# the broccoli table also has to be modified...
-df$Scaffold[df$Species==sp_names[3]] = paste0(sp_names[3], '@',
-  df$Scaffold[df$Species==sp_names[3]])
-df$Scaffold[df$Species==sp_names[4]] = paste0(sp_names[4], '@',
-  df$Scaffold[df$Species==sp_names[4]])
-# remove inputted header from idx_col and idx_lss
+# add these new scaffold names to the table,
+# substituting the previous sequid names...
+df_input_table$Scaffold[df_input_table$Species==sp_names[3]] = paste0(sp_names[3], '@',
+  df_input_table$Scaffold[df_input_table$Species==sp_names[3]])
+df_input_table$Scaffold[df_input_table$Species==sp_names[4]] = paste0(sp_names[4], '@',
+  df_input_table$Scaffold[df_input_table$Species==sp_names[4]])
+# remove inputted header from idx_col and idx_lss (complete species name...)
 idx_col = idx_col[-1,]
 idx_lss = idx_lss[-1,]
-highlighting_crm$crm = idx_col$crm  # pair colours with crm
 # row bind both indexes into a single data.frame
 # colour had to be removed in order to have same columns
 idx = rbind(idx_col, idx_lss)
 # remove all orthologs that are not included in the index
 # (if minor scaffolds are not in index, remove orthologs of minor scaffolds from `df`)
-df = df[df$Scaffold %in% idx$crm,]
+df_input_table = df_input_table[df_input_table$Scaffold %in% idx$crm,]
+# pair colours from `highlighting_crm` with sequids
+highlighting_crm$sequid = idx_col$crm
 
 
 ### CREATE GENOMIC LINKS and POINTS DATAFRAMES ###
+
+# function to add members of OG to `df_links` or `df_points`
+linking <- function() {
+  if (nrow(members) == 2) {
+    # two members: we can create a single link between
+    # the pair of coordinates
+    o = TRUE
+  } else {
+    # (three) members: add them to both dataframes (`df_points` and `df_links`)
+    # they can be included in either data.frame, depending on the
+    # desired plot.
+    o = FALSE
+  }
+    new_row = data.frame(
+      sequid_col = members_col$Scaffold,
+      start_col = members_col$GeneStart,
+      end_col = members_col$GeneEnd,
+      sequid_lss = members_lss$Scaffold,
+      start_lss = members_lss$GeneStart,
+      end_lss = members_lss$GeneEnd,
+      one_to_one = o
+    )
+  # evaluate OG quality and assign it an arbitrary number
+  # from 1 (best) to 5 (worst)
+  ##if () {}
+  # append new row to links df:
+  return(rbind(df_links, new_row))}
+
+pointing <- function() {
+    # if they are a many-members orthogroup, plot as genomic points:
+    # try to colour each point depending on which species
+    # has more genes in the selected orthogroup
+    # (compare nrows of `members_lss` versus `members_col`)
+  
+  # track two to one orthologs, so they can be added and
+  # removed from CIRCOS plots easily.
+  if (nrow(members)==3) {
+    to = TRUE
+  } else {
+    to = FALSE
+  }
+  if (nrow(members_lss)==nrow(members_col)) {
+    # the same amount of OG members (genes) in both species.
+    col = 'yellow'
+  } else if (nrow(members_lss) > nrow(members_col)) {
+    # more OG members (genes) in species colourless (dcat)
+    col = c(rep('red', nrow(members_col)), rep('green', nrow(members_lss)))
+  } else if (nrow(members_lss)< nrow(members_col)) {
+    # more OG members (genes) in species colourful (dtil)
+    col = c(rep('green', nrow(members_col)), rep('red', nrow(members_lss)))
+  }
+  new_rows = data.frame(
+    sequid = c(members_col$Scaffold, members_lss$Scaffold),
+    start = c(members_col$GeneStart, members_lss$GeneStart),
+    end = c(members_col$GeneEnd, members_lss$GeneEnd),
+    col = col,
+    ogid = c(members_col$OGid, members_lss$OGid), #DEBUG
+    two_to_one = to
+  )
+  return(rbind(df_points, new_rows))}
 
 # resulting df with genomic links
 df_links = data.frame()
@@ -85,87 +156,66 @@ df_links = data.frame()
 df_points = data.frame()
 # keep track of removed and included OGs in analyses:
 og.removed = 0 ; og.included = 0
+
 # fill previous df with input df:
-for (og in unique(df$OGid)) {
+for (og in unique(df_input_table$OGid)) {
   # select all members of orthogroup
-  members = df[df$OGid==og,]
-  ##print(members[,c('OGtype', 'Scaffold', 'GeneStart', 'GeneEnd', 'Species', 'OGid')])#DEBUG
+  members = df_input_table[df_input_table$OGid==og,]
+  # subset all members from OG, for species `col` and `lss`
   members_col = members[members$Species==sp_names[3],]
   members_lss = members[members$Species==sp_names[4],]
-
+  og.included = og.included+1
   if (nrow(members_lss)==0 | nrow(members_col)==0) {
     # it is possible to remove all genes from a family while
     # deleting rows located at minor scaffolds
     # just skip these cases...
     og.removed = og.removed+1
+    og.included = og.included-1
+    if (og.removed < 2) {
     print(paste0('WARNING: ', og, ' has been removed because',
-    ' at least one species has all of their orthologs outside provided index sequids.'))
-  } else if (nrow(members)==2) {
-    og.included = og.included+1
-    # two members: we can create a link between coordinates
-    # maybe even 3...
-    new_row = data.frame(
-      sequid_col = members_col$Scaffold,
-      start_col = members_col$GeneStart,
-      end_col = members_col$GeneEnd,
-      sequid_lss = members_lss$Scaffold,
-      start_lss = members_lss$GeneStart,
-      end_lss = members_lss$GeneEnd
-    )
-    # append new row to links df:
-    df_links = rbind(df_links, new_row)
-  } else if (nrow(members_lss)==nrow(members_col)) {
-    og.included = og.included+1
-    # if they are a many-members orthogroup, plot as genomic points:
-    # try to colour each point depending on which species
-    # has more genes in the selected orthogroup
-    # (compare nrows of `members_lss` versus `members_col`)
-      new_rows = data.frame(
-        sequid = members$Scaffold,
-        start = members$GeneStart,
-        end = members$GeneEnd,
-        col = 'yellow',
-        ogid = members$OGid
-      )
-      #print(new_rows)#DEBUG
-      df_points = rbind(df_points, new_rows)
-  } else if (nrow(members_lss) >nrow(members_col)) {
-    og.included = og.included+1
-    # more genes in species colourless (dcat)
-      new_rows = data.frame(
-        sequid = c(members_col$Scaffold, members_lss$Scaffold),
-        start = c(members_col$GeneStart, members_lss$GeneStart),
-        end = c(members_col$GeneEnd, members_lss$GeneEnd),
-        col = c(rep('red', nrow(members_col)), rep('green', nrow(members_lss))),
-        ogid = c(members_col$OGid, members_lss$OGid) #DEBUG
-      )
-      #print(new_rows)#DEBUG
-      df_points = rbind(df_points, new_rows)
-  } else if (nrow(members_lss)< nrow(members_col)) {
-    og.included = og.included+1
-    # more genes in species colourful (dtil)
-      new_rows = data.frame(
-        sequid = c(members_col$Scaffold, members_lss$Scaffold),
-        start = c(members_col$GeneStart, members_lss$GeneStart),
-        end = c(members_col$GeneEnd, members_lss$GeneEnd),
-        col = c(rep('green', nrow(members_col)), rep('red', nrow(members_lss))),
-        ogid = c(members_col$OGid, members_lss$OGid) #DEBUG
-      )
-      #print(new_rows)#DEBUG
-      df_points = rbind(df_points, new_rows)
+    ' at least one species has all of their orthologs outside provided index sequids\n',
+    '(probably all genes from OG were in minor scaffolds)'))
+    } else {
+    print(paste0('WARNING: ', og, ' removed (minor scaffolds)'))
+    }
+  } else if (nrow(members) == 2) {
+    # two members: only add them to `df_links`
+    # do not consider them as multigenic (`df_points`)
+    df_links = linking()
+  } else if (nrow(members) == 3) {
+    # three members: add them to both dataframes...
+    # they can be included in either data.frame, and called to different
+    # desired plots depending on the tag. add tags to compartimentalize
+    # plotting (multiple plots from the same data)
+    df_links = linking()
+    df_points = pointing()
+  } else {
+    df_points = pointing()
   }
 }
+
 print(paste('OGs included:', og.included))
-print(paste('OGs removed:', og.removed))
-print(paste('Overall OGs:', length(unique(df$OGid))))
+print(paste('OGs removed (minor scaffolds?):', og.removed))
+print(paste('Overall OGs:', length(unique(df_input_table$OGid))))
+##print('--> DEBUG: `df_links` and `df_points` head')
+##print(head(df_points))
+##print(head(df_links))
+
 # add colours to df_links depending on idx_col of 'origin'
+# there is no need to manually create a palette, colour_values will take care of it
 df_links$col = 0
-window_size = 1e7
+df_links$col_qual = 0
 for (i in c(1:nrow(highlighting_crm))) {
-  df_links$col[df_links$sequid_col==highlighting_crm[i, 2]]=highlighting_crm[i, 1]
+  # add primary colours column
+  df_links$col[df_links$sequid_col==highlighting_crm[i, 1] & df_links$one_to_one==TRUE] = highlighting_crm[i, 2]
+  # add secondary colour column
+  df_links$col[df_links$sequid_col==highlighting_crm[i, 1] & df_links$one_to_one==FALSE] = highlighting_crm[i, 3]
 }
-# create lines instead of genomicPoints()
-# the df_points will have to be formatted differently (create df_lines)
+print("--> DEBUG: `highlighting_crm` head()")
+print(head(highlighting_crm))
+
+# reformat `df_points` into `df_lines`
+# create genomicLines() instead of genomicPoints()
 df_lines=data.frame()
 for (c in idx$crm) {
   e = idx$end[idx$crm == c]
@@ -193,6 +243,7 @@ for (c in idx$crm) {
     amount_g = nrow(df_sel[df_sel$col=='green', ]))
 df_lines = rbind(df_lines, new_rows)
 }
+# compute the range of data (from 0 to max(df))
 track_ylim = max(c(df_lines$amount_y,
                    df_lines$amount_r,
                    df_lines$amount_g))
@@ -202,7 +253,7 @@ print(paste('Most genes in `df_lines.yellow`:', max(df_lines$amount_y)))
 print(head(n=20, df_lines)) #DEBUG
 
 
-### PLOTTING CIRCOS FIGURE ###
+### PLOTTING 'STANDARD' CIRCOS FIGURE ###
 
 # # # # # #
 # export plot to 'Rplot.pdf'
@@ -220,7 +271,7 @@ circos.par(gap.degree = c(rep(1, nrow(idx_col)-1), 5,
 circos.par(cell.padding = c(0.02, 0, 0.02, 0))
 # initialize with sectors <- chromosome names
 circos.initialize(sectors=idx$crm,
-  xlim=idx[, c(2,3)])
+  xlim=idx[, c('start', 'end')])
 circos.track(idx$crm, ylim=c(0, track_ylim),
   track.height=.15, #bg.border=NA,
   panel.fun  = function(x,y) {
@@ -228,15 +279,10 @@ circos.track(idx$crm, ylim=c(0, track_ylim),
     cllcrm = CELL_META$sector.index
     xlim   = CELL_META$xlim
     ylim   = CELL_META$ylim
-    # draw points where members of multigenic orthogroups are at
-    # colour points by more/less/equal members in orthogroup for both species
-    ##circos.genomicPoints(df_points[df_points$sequid==cllcrm, c('start', 'end')],
-    ##  CELL_META$ycenter,  # place points in the Y-center of the cell
-    ##  col=df_points[df_points$sequid==cllcrm, 'col'],
-    ##  pch=16, cex=2) # make them big and round dots
     # instead of points, draw multigenic orthogroups as
     # repetitive elements would be drawn
-    d = df_lines[df_lines$sequid==cllcrm, ]
+    # this paragraph incorporates 2:1 OGs as links (remove from points)...
+    d = df_lines[df_lines$sequid==cllcrm & df_lines$two_to_one==FALSE, ]
     circos.genomicLines(d[, c('start', 'end')],
     d[, 'amount_g'], type='segment', col='green')
     circos.genomicLines(d[, c('start', 'end')],
@@ -259,6 +305,7 @@ circos.genomicLink(df_links[, c(1:3)], df_links[, c(4:6)],
 #    legend = c(val_range$min[i], val_range$max[i]),
 #    fill = c(col_range[1], col_range[2]),
 #    )
+title('Includes 2:1 orthologs (darker links)')
 
 
 ### PLOTTING LINKS FROM A SINGLE CHROMOSOME AT A TIME ###
@@ -268,6 +315,48 @@ for (sequid in idx_col$crm) {
 # filter out all links that are not in `sequid`
 # each iteration will create a plot with links from a single sequid
 links = df_links[df_links$sequid_col == sequid, ]
+circos.clear()
+circos.par(gap.degree = c(rep(1, nrow(idx_col)-1), 5,
+  rep(1, nrow(idx_lss)-1), 5))
+circos.par(cell.padding = c(0.02, 0, 0.02, 0))
+circos.initialize(sectors=idx$crm,
+  xlim=idx[, c(2,3)])
+circos.track(idx$crm, ylim=c(0, track_ylim),
+  track.height=.15, #bg.border=NA,
+  panel.fun  = function(x,y) {
+    # shorthands for a few sector properties
+    cllcrm = CELL_META$sector.index
+    xlim   = CELL_META$xlim
+    ylim   = CELL_META$ylim
+    d = df_lines[df_lines$sequid==cllcrm & df_lines$two_to_one==FALSE, ]
+    circos.genomicLines(d[, c('start', 'end')],
+    d[, 'amount_g'], type='segment', col='green')
+    circos.genomicLines(d[, c('start', 'end')],
+    d[, 'amount_y'], type='segment', col='yellow')
+    circos.genomicLines(d[, c('start', 'end')],
+    d[, 'amount_r'], type='segment', col='red')
+    circos.text(mean(xlim), mean(ylim)+mm_y(9), # add Y space (chr.name above track)
+      gsub(".*d_", "", cllcrm),  # crm name that will be displayed
+      # the global substitution (gsub) can trim the sp identifier or 'Scaffold' entirely
+      cex=1.2, col='black', facing='inside', niceFacing=TRUE)
+  }
+)
+# connect 1:3 (origin segment) to 4:6 (recipient segment)
+circos.genomicLink(links[, c(1:3)], links[, c(4:6)],
+  col = links[, 'col'], #h.ratio = .4
+)
+# add a legend with the min and max of misc value,
+# and the colours used to represent these.
+#  legend('bottomleft', title=leg_titles[i], bty='n',
+#    legend = c(val_range$min[i], val_range$max[i]),
+#    fill = c(col_range[1], col_range[2]),
+#    )
+title('Includes 2:1 orthologs (darker links)')
+}  # acaba les iteracions de subseccionar les unions cromosomals
+
+
+### REMOVING 2:1 FROM THE CENTER LINKS ('STANDARD')
+
 circos.clear()
 circos.par(gap.degree = c(rep(1, nrow(idx_col)-1), 5,
   rep(1, nrow(idx_lss)-1), 5))
@@ -295,8 +384,10 @@ circos.track(idx$crm, ylim=c(0, track_ylim),
   }
 )
 # connect 1:3 (origin segment) to 4:6 (recipient segment)
-circos.genomicLink(links[, c(1:3)], links[, c(4:6)],
-  col = links[, 'col'], #h.ratio = .4
+# remove 2:1 links from plot (they are already incorporated in outside ring lines)
+d = df_links[df_links$one_to_one==TRUE]
+circos.genomicLink(d[, c(1:3)], d[, c(4:6)],
+  col = d[, 'col'], #h.ratio = .4
 )
 # add a legend with the min and max of misc value,
 # and the colours used to represent these.
@@ -304,7 +395,7 @@ circos.genomicLink(links[, c(1:3)], links[, c(4:6)],
 #    legend = c(val_range$min[i], val_range$max[i]),
 #    fill = c(col_range[1], col_range[2]),
 #    )
-}  # acaba les iteracions de subseccionar les unions cromosomals
+title('Removing 2:1 links and incorporating them "inside" scaffolds')
 
 
 ### REMOVING *OUTLIER* MULTIGENIC FAMILIES FROM THE PLOT ###
@@ -347,8 +438,10 @@ circos.track(idx$crm, ylim=c(0, track_ylim),
   }
 )
 # connect 1:3 (origin segment) to 4:6 (recipient segment)
-circos.genomicLink(df_links[, c(1:3)], df_links[, c(4:6)],
-  col = df_links[, 'col'], #h.ratio = .4
+# remove 2:1 links from plot (they are already incorporated in outside ring lines)
+d = df_links[df_links$one_to_one==TRUE]
+circos.genomicLink(d[, c(1:3)], d[, c(4:6)],
+  col = d[, 'col'], #h.ratio = .4
 )
 # add a legend with the min and max of misc value,
 # and the colours used to represent these.
@@ -356,6 +449,7 @@ circos.genomicLink(df_links[, c(1:3)], df_links[, c(4:6)],
 #    legend = c(val_range$min[i], val_range$max[i]),
 #    fill = c(col_range[1], col_range[2]),
 #    )
+title('Removing 2:1 links and the top 3 densest multigenic regions from inner-scaffolds plot')
 
 
 # # # # # #
