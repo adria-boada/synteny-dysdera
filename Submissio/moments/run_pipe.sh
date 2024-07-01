c55de62df2e1f5f1f00f4c56ce7f8869a3d2024b
adria-boada <94541540+adria-boada@users.noreply.github.com>
2024-07-01 15:26:14 +0200

    Hotfix3

--- Submissio/moments/run_pipe.sh
+++ Submissio/moments/run_pipe.sh
@@ -5,23 +5,19 @@
 ## usage: bash run_pipe.sh (without 'qsub')
 
 source /users-d3/adria.boada/.bash_profile
-# Activa el paquet de Python3 de 'moments'
-eval "$(conda shell.bash hook)"
-conda activate moments
-# Revisa la versió de Python3 i els paquets disponibles.
-echo $(python3 --version)
-echo $(python3 -c 'import moments')
-# Podries fer un script que importi tots els paquets
-# i n'asseguri el correcte funcionament.
 
+# Command-line parameters.
 QSUB_OPTIONFILE="$1"
 PYT_PARAM_FILE="$2"
+
+# Utilizem un 'environment' de Python amb els paquets necessaris preparats.
+PYT_BIN="./anaconda3/envs/moments/bin/python"
 
 i=0
 cat $PYT_PARAM_FILE | while read line; do
 	# Un nom de fitxer per a cada ordre qsub.
 	STDOUT="$(date +%y%m%d-%H%M)-moments$i.log"
-	qsub -@ "$QSUB_OPTIONFILE" -N "moments$i" -o "$STDOUT" -j yes -b y python3 "$line"
+	qsub -@ "$QSUB_OPTIONFILE" -N "moments$i" -o "$STDOUT" -j yes -b y "$PYT_BIN" "$line"
 	i=$(expr $i + 1)
 done
 
