53ac8614f27c73e932635d1ef161e27046872fef
adria-boada <94541540+adria-boada@users.noreply.github.com>
2024-07-01 12:58:49 +0200

    Hotfix2

--- Submissio/moments/run_pipe.sh
+++ Submissio/moments/run_pipe.sh
@@ -4,8 +4,9 @@
 
 ## usage: bash run_pipe.sh (without 'qsub')
 
-source $HOME/../.bash_profile
+source /users-d3/adria.boada/.bash_profile
 # Activa el paquet de Python3 de 'moments'
+eval "$(conda shell.bash hook)"
 conda activate moments
 # Revisa la versió de Python3 i els paquets disponibles.
 echo $(python3 --version)
@@ -19,7 +20,7 @@ PYT_PARAM_FILE="$2"
 i=0
 cat $PYT_PARAM_FILE | while read line; do
 	# Un nom de fitxer per a cada ordre qsub.
-	STDOUT="$(date +%y%m%d-%H%M)-moments.log"
+	STDOUT="$(date +%y%m%d-%H%M)-moments$i.log"
 	qsub -@ "$QSUB_OPTIONFILE" -N "moments$i" -o "$STDOUT" -j yes -b y python3 "$line"
 	i=$(expr $i + 1)
 done
